a2d9cf584347de039dd2ad196c8af0bf00c3c0d2
saihe <saihe.kyon@gmail.com>
2016-12-03 00:43:23 +0900

    トークンは取れた。 コルドバプラグインにappbrowser追加。

--- www/lib/3rd/openfb.js
+++ www/lib/3rd/openfb.js
@@ -204,8 +204,10 @@ var openFB = (function () {
             token = tokenStore.fbAccessToken;
 
         /* Remove token. Will fail silently if does not exist */
-        tokenStore.removeItem('fbtoken');
-
+        //tokenStore.removeItem('fbtoken');
+        //変更
+        tokenStore.removeItem('fbAccessToken');
+        
         if (token) {
             logoutWindow = window.open(logoutURL + '?access_token=' + token + '&next=' + logoutRedirectURL, '_blank', 'location=no,clearcache=yes');
             if (runningInCordova) {
